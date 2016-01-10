f691b3665f2e6140f26a5d778dbdf3cbd86fe737
Hyeon Kim <simnalamburt@gmail.com>
2016-01-11 02:08:45 +0900

    Webpack에 uglify, source map 붙임

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,12 @@
+var webpack = require("webpack");
+
+var optUglify = { compress: { warnings: false } };
+
 module.exports = {
   entry: './build/main.js',
-  output: { path: 'build', filename: '_bundle.js' }
+  output: { path: 'build', filename: '_bundle.js' },
+  devtool: 'source-map',
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin(optUglify)
+  ]
 };
